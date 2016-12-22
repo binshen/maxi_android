da70b8eafc59281d5b972cfc2e3e8f08653d90ef
沈斌 <bluelazysb@hotmail.com>
2016-12-22 08:18:59 +0800

    updates

--- app/src/main/java/com/maxi/waterpurifier/fragment/HomeFragment.java
+++ app/src/main/java/com/maxi/waterpurifier/fragment/HomeFragment.java
@@ -10,12 +10,10 @@ import android.view.ViewGroup;
 import com.daimajia.slider.library.Indicators.PagerIndicator;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.maxi.waterpurifier.R;
 import com.maxi.waterpurifier.base.BaseFragment;
-
-import java.util.HashMap;
 
 public class HomeFragment extends BaseFragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
@@ -36,22 +34,34 @@ public class HomeFragment extends BaseFragment implements BaseSliderView.OnSlide
 
         mSlider = (SliderLayout) view.findViewById(R.id.slider);
 
-        HashMap<String,String> url_maps = new HashMap<String, String>();
-        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
-        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
-        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
-        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+//        HashMap<String,String> url_maps = new HashMap<String, String>();
+//        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+//        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+//        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+//        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+//        for(String name : url_maps.keySet()){
+//            TextSliderView textSliderView = new TextSliderView(getContext());
+//            textSliderView.image(url_maps.get(name));
+//            textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+//            mSlider.addSlider(textSliderView);
+//        }
 
-        for(String name : url_maps.keySet()){
-            TextSliderView textSliderView = new TextSliderView(getContext());
-            //textSliderView.description(name);
-            textSliderView.image(url_maps.get(name));
-            textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
-            //textSliderView.bundle(new Bundle());
-            //textSliderView.getBundle().putString("extra",name);
-            //textSliderView.setOnSliderClickListener(this);
-            mSlider.addSlider(textSliderView);
-        }
+        DefaultSliderView sliderView1 = new DefaultSliderView(getContext());
+        sliderView1.image(R.drawable.home_slider1);
+        sliderView1.setScaleType(BaseSliderView.ScaleType.Fit);
+        mSlider.addSlider(sliderView1);
+        DefaultSliderView sliderView2 = new DefaultSliderView(getContext());
+        sliderView2.image(R.drawable.home_slider1);
+        sliderView2.setScaleType(BaseSliderView.ScaleType.Fit);
+        mSlider.addSlider(sliderView2);
+        DefaultSliderView sliderView3 = new DefaultSliderView(getContext());
+        sliderView3.image(R.drawable.home_slider1);
+        sliderView3.setScaleType(BaseSliderView.ScaleType.Fit);
+        mSlider.addSlider(sliderView3);
+        DefaultSliderView sliderView4 = new DefaultSliderView(getContext());
+        sliderView4.image(R.drawable.home_slider1);
+        sliderView4.setScaleType(BaseSliderView.ScaleType.Fit);
+        mSlider.addSlider(sliderView4);
 
         mSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
         mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
