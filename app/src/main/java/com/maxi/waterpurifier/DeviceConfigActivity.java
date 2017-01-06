fbb10aa9775f1bd34ce4c95354d287821d222b7a
沈斌 <bluelazysb@hotmail.com>
2017-01-06 13:38:51 +0800

    updates

--- app/src/main/java/com/maxi/waterpurifier/DeviceConfigActivity.java
+++ app/src/main/java/com/maxi/waterpurifier/DeviceConfigActivity.java
@@ -1,5 +1,6 @@
 package com.maxi.waterpurifier;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,6 +10,10 @@ import android.widget.TextView;
 import com.maxi.waterpurifier.base.BaseActivity;
 
 public class DeviceConfigActivity extends BaseActivity implements View.OnClickListener {
+
+    private RelativeLayout mRlIconDeviceLogo;
+    private RelativeLayout mRlIconDeviceRecharge;
+    private ImageView mIvBtnDeviceUnbind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,13 @@ public class DeviceConfigActivity extends BaseActivity implements View.OnClickLi
         TextView mTvHeader = (TextView) findViewById(R.id.tv_head_title);
         mTvHeader.setText("系统设置");
         mTvHeader.setTextColor(getResources().getColor(R.color.white));
+
+        mRlIconDeviceLogo = (RelativeLayout) findViewById(R.id.rl_icon_device_logo);
+        mRlIconDeviceLogo.setOnClickListener(this);
+        mRlIconDeviceRecharge = (RelativeLayout) findViewById(R.id.rl_icon_device_recharge);
+        mRlIconDeviceRecharge.setOnClickListener(this);
+        mIvBtnDeviceUnbind = (ImageView) findViewById(R.id.iv_btn_device_unbind);
+        mIvBtnDeviceUnbind.setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +43,17 @@ public class DeviceConfigActivity extends BaseActivity implements View.OnClickLi
         switch (v.getId()) {
             case R.id.btn_head_left:
                 finish();
+                break;
+
+            case R.id.rl_icon_device_logo:
+                startActivity(new Intent(this, DeviceUpdateNameActivity.class));
+                break;
+
+            case R.id.rl_icon_device_recharge:
+                startActivity(new Intent(this, DeviceRechargeActivity.class));
+                break;
+
+            case R.id.iv_btn_device_unbind:
                 break;
         }
     }
