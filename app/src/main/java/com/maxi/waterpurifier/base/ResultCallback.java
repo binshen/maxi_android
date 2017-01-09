59a039d381fff5acbb81abb654100296b868fa09
沈斌 <bluelazysb@hotmail.com>
2017-01-09 13:41:49 +0800

    updates

--- app/src/main/java/com/maxi/waterpurifier/base/ResultCallback.java
+++ app/src/main/java/com/maxi/waterpurifier/base/ResultCallback.java
@@ -1,50 +1,28 @@
 package com.maxi.waterpurifier.base;
 
-import com.google.gson.Gson;
 import com.zhy.http.okhttp.callback.Callback;
 
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 import okhttp3.Response;
 
 /**
- * Created by bin.shen on 2017/1/8.
+ * Created by bin.shen on 02/01/2017.
  */
-public abstract class ResultCallback<T> extends Callback<Result<T>> {
-    @Override
-    public Result<T> parseNetworkResponse(Response response, int id) throws Exception {
-        String string = response.body().string();
 
-        Gson gson = new Gson();
-        Type objectType = type(Result.class, getClazz());
-        return gson.fromJson(string, objectType);
+public abstract class ResultCallback<T> extends Callback<Result<T>> {
+
+    @Override
+    public Result parseNetworkResponse(Response response, int id) throws IOException {
+        return Result.fromJson(response.body().string(), getClazz());
     }
 
-    /**
-     * 获取T代表的class类型
-     * @return
-     */
     public Class<T> getClazz() {
         Type t = getClass().getGenericSuperclass();
         ParameterizedType p = (ParameterizedType) t;
-        Class<T> c = (Class<T>) p.getActualTypeArguments()[0];
-        return c;
-    }
-
-    static ParameterizedType type(final Class raw, final Type... args) {
-        return new ParameterizedType() {
-            public Type getRawType() {
-                return raw;
-            }
-
-            public Type[] getActualTypeArguments() {
-                return args;
-            }
-
-            public Type getOwnerType() {
-                return null;
-            }
-        };
+        return (Class<T>) p.getActualTypeArguments()[0];
     }
 }
+
