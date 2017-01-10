e21ecb0dcda1f18705cd438f91689f8ebbbbb343
沈斌 <bluelazysb@hotmail.com>
2017-01-10 10:33:18 +0800

    updates

--- app/src/main/java/com/maxi/waterpurifier/DeviceAddActivity.java
+++ app/src/main/java/com/maxi/waterpurifier/DeviceAddActivity.java
@@ -1,7 +1,16 @@
 package com.maxi.waterpurifier;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -10,7 +19,12 @@ import com.maxi.waterpurifier.base.BaseActivity;
 
 public class DeviceAddActivity extends BaseActivity implements View.OnClickListener {
 
+    private EditText mEtWifiSid;
+    private EditText mEtWifiPsw;
+
     private ImageView mIvBtnDeviceNext;
+
+    private BroadcastReceiver mWifiChangedReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,50 @@ public class DeviceAddActivity extends BaseActivity implements View.OnClickListe
 
         mIvBtnDeviceNext = (ImageView) findViewById(R.id.iv_btn_device_next);
         mIvBtnDeviceNext.setOnClickListener(this);
+
+        mEtWifiSid = (EditText) findViewById(R.id.et_wifi_sid);
+        mEtWifiPsw = (EditText) findViewById(R.id.et_wifi_psw);
+
+        mEtWifiSid.setText(getSSid());
+        mWifiChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    mEtWifiSid.setText(getSSid());
+                    mEtWifiPsw.requestFocus();
+                    mIvBtnDeviceNext.setEnabled(true);
+                }else {
+                    mEtWifiSid.setText("");
+                    mEtWifiSid.requestFocus();
+                    mIvBtnDeviceNext.setEnabled(false);
+                    if (mLoadDialog.isShowing()) {
+                        mLoadDialog.dismiss();
+                    }
+                }
+            }
+        };
+        registerReceiver(mWifiChangedReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    private String getSSid(){
+
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        if(wm != null){
+            WifiInfo wi = wm.getConnectionInfo();
+            if(wi != null){
+                String ssid = wi.getSSID();
+                if(ssid.length() > 2 && ssid.startsWith("\"") && ssid.endsWith("\"")){
+                    return ssid.substring(1, ssid.length() - 1);
+                }else if(ssid.equals("<unknown ssid>")) {
+                    return "";
+                } else {
+                    return ssid;
+                }
+            }
+        }
+        return "";
     }
 
     @Override
@@ -41,6 +99,16 @@ public class DeviceAddActivity extends BaseActivity implements View.OnClickListe
             case R.id.iv_btn_device_next:
                 finish();
                 break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            unregisterReceiver(mWifiChangedReceiver);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
